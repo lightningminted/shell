62dcb3d5f4f9135b5e9cbf2a4239550a19c74f52
lightningminted <matthew.k.tamura@gmail.com>
2026-05-05 14:01:37 -0400

    small update to assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,14 +43,14 @@ mkdir data/processed
 mkdir data/processed/server_logs data/processed/user_logs  data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-mv data/raw/server_log_* data/processed/server_logs
+cp data/raw/server_log_* data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-mv data/raw/user_log_* data/processed/user_logs
-mv data/raw/event_log_* data/processed/event_logs
+cp data/raw/user_log_* data/processed/user_logs
+cp data/raw/event_log_* data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm data/raw/*ipaddr*
+rm data/raw/*ipaddr* data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch data/inventory.txt
